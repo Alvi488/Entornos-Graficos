ae95feebfaec3af41edfd162aea385a38f37e68e
AlviNotebook <alvaroyaugusto@gmail.com>
2024-10-24 13:43:21 -0300

    ej4

--- Practica/7-SesionesYCookies-PHP/Ejercicio-3/nombre_usuario.php
+++ Practica/7-SesionesYCookies-PHP/Ejercicio-3/nombre_usuario.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION["nombre_usuario"])){
 }
 ?>
 <!DOCTYPE HTML>
-<html lang="en">
+<html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
